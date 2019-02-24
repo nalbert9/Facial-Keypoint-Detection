b05316d9f05b641684164dfd76c25d586553f9fb
gfoxx29 <nalbert9@hotmail.fr>
2019-02-24 10:26:26 +0100

    Improved Model Architecture

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-## TODO: define the convolutional neural network architecture
+## Define the convolutional neural network architecture
 
 import torch
 import torch.nn as nn
@@ -6,18 +6,16 @@ import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
 import torch.nn.init as I
 
-
 class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
         
-        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## Define all the layers of this CNN, the only requirements are:
         ## 1. This network takes in a square (same width and height), grayscale image as input
         ## 2. It ends with a linear layer that represents the keypoints
-        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        ## Last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
         
-        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         
         ## Shape of a Convolutional Layer
@@ -50,6 +48,7 @@ class Net(nn.Module):
         # output size = (W-F)/S +1 = (54-3)/1 + 1 = 52
         # the output Tensor for one image, will have the dimensions: (64, 54, 54)
         self.conv3 = nn.Conv2d(64, 128, 3)
+        self.conv3_bn = nn.BatchNorm2d(128)
         
         # output size = (W-F)/S +1 = (26-3)/1 + 1 = 24
         # the output Tensor for one image, will have the dimensions: (128, 26, 26)
@@ -64,19 +63,15 @@ class Net(nn.Module):
         
         # Fully-connected (linear) layers
         self.fc1 = nn.Linear(512*6*6, 1024)
-        self.fc2 = nn.Linear(1024, 1024)
-        self.fc3 = nn.Linear(1024, 68*2)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 68*2)
         
         # Dropout
-        self.dropout = nn.Dropout(p=0.5)
+        self.dropout = nn.Dropout(p=0.25)
         
-        ## Note that among the layers to add, consider including:
-        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        
-
         
     def forward(self, x):
-        ## TODO: Define the feedforward behavior of this model
+        ## Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         
         # 5 conv/relu + pool layers
@@ -89,13 +84,11 @@ class Net(nn.Module):
         # Prep for linear layer / Flatten
         x = x.view(x.size(0), -1)
         
-        # two linear layers with dropout in between
+        # linear layers with dropout in between
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
         x = F.relu(self.fc2(x))
         x = self.dropout(x)
         x = self.fc3(x)
         
-        # a modified x, having gone through all the layers of your model, should be returned
-        # final output
         return x
